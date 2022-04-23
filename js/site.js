ec0a8606c66419c96fa74e79bfebfa6e9257b805
VJ <twjmy@msn.com>
2022-04-24 00:23:04 +0800

    💬Messenger chat plugin

--- js/site.js
+++ js/site.js
@@ -34,3 +34,22 @@ jQuery(document).ready(function($) {
         return false;
     });
 });
+
+// Messenger 洽談外掛程式 SDK code
+var chatbox = document.getElementById('fb-customer-chat');
+chatbox.setAttribute("page_id", "628293563853646");
+chatbox.setAttribute("attribution", "biz_inbox");
+window.fbAsyncInit = function () {
+    FB.init({
+        xfbml: true,
+        version: 'v13.0'
+    });
+};
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/zh_TW/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
